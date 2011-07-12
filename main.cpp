b70d84befbc7d59f8b3c9616ea52d1bd160527db
QIvan <i.zemlyansky@vniins.ru>
2011-07-12 19:23:51 +0400

    Первый коммит. Все файлы работы с БД выделены в отдельный проект.

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <QApplication>
+#include <QtTest>
+#include <stdio.h>
+
+#include "db.h"
+
+
+using namespace std;
+
+/* Debugging level, set with --debug-level command like option.
+   DBG_PRINTF() macro calls cause a display if their
+   level (first argument) is lower or equal than global_debug_level.
+   Normally the values should be:
+   1: signal something that reveals an inconsistency inside our code
+
+   2: signal something that shouldn't normally happen but may happen under certain circumstances (for example severe violations of RFC in the data).
+
+   3 and higher: used for actual printf-like debugging. Use higher
+   values for the more mundane messages or those that cause a lot of output
+*/
+int global_debug_level;
+
+
+QString gl_xpm_path; // used by FT_MAKE_ICON (icons.h)
+QString gl_tr_path;
+QString gl_help_path;
+
+void
+debug_printf(int level, const char* file, int line, const char *fmt, ...)
+{
+  if (level<=global_debug_level) {
+    fprintf(stderr, "%s, line %d: ", file, line);
+    va_list ap;
+    va_start(ap,fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fprintf(stderr, "\n");
+    fflush(stderr);
+  }
+
+}
+
+void
+err_printf(const char* file, int line, const char *fmt, ...)
+{
+  fprintf(stderr, "%s, line %d: ERR: ", file, line);
+  va_list ap;
+  va_start(ap,fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  fprintf(stderr, "\n");
+  fflush(stderr);
+}
+
+
+void
+usage(const char* progname)
+{
+#ifdef Q_WS_WIN
+  QMessageBox::critical(NULL, "Arguments error", "The possible arguments are --dbcnx followed by a connection string and --config followed by a configuration name.\nExample, --dbcnx \"dbname=mail user=mailadmin host=pgserver\" --config myconf");
+#else
+  fprintf(stderr, "Usage: %s [--debug-output=level] [--config=confname] \"connect string\".\nThe connect string looks like, for example, \"dbname=mail user=mailadmin host=pgserver\".\n",
+          progname);
+#endif
+  exit(1);
+}
+
+
+#ifdef IS_TEST
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    return 0;
+}
+#endif
---
+++ manitouPortotype.pro
@@ -0,0 +1,37 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-07-01T16:06:54
+#
+#-------------------------------------------------
+
+QT       += core, gui, network, testlib
+
+TARGET = manitouPortotype
+CONFIG   += console
+DEFINES  = WITH-PQSQL=1
+DEFINES  += IS_TEST=1, IS_ALL_TEST=0
+LIBS = -L/usr/lib64/ -lpq
+
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    sqlstream.cpp \
+    sqlquery.cpp \
+    db.cpp \
+    db_listener.cpp \
+    date.cpp \
+    addresses.cpp \
+
+HEADERS += \
+    sqlstream.h \
+    sqlquery.h \
+    dbtypes.h \
+    db_listener.h \
+    main.h \
+    db.h \
+    config.h \
+    database.h \
+    date.h \
+    addresses.h \
