1b0a8ee8d94f4033b1279b92ac5391aa9325e230
QIvan <i.zemlyansky@vniins.ru>
2011-07-21 15:49:29 +0400

    Поправлен тест на sql_write_fields, добавлена новая тестовая функция генерирующая исключения.

--- test/testSqlWriteFields.cpp
+++ test/testSqlWriteFields.cpp
@@ -3,13 +3,20 @@
 #include "RegistryTest.h"
 #include "sqlstream.h"
 #include "sqlquery.h"
+#include "date.h"
 
 std::string TestNames::sql_wirte_fields() { return "sql_wirte_fields"; }
-
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(test_sql_write_fields, TestNames::sql_wirte_fields());
+
 
 test_sql_write_fields::test_sql_write_fields() : m_DB(new db_cnx)
 {
+}
+
+void test_sql_write_fields::setUp()
+{
+    sql_stream query("DELETE FROM test_table", *m_DB);
+    sql_stream reset_id("ALTER SEQUENCE sequence_id RESTART with 2", *m_DB);
 }
 
 void test_sql_write_fields::insert()
@@ -41,22 +48,25 @@ void test_sql_write_fields::insert()
                 .arg(fiedls2.values());
         sql_stream insert2(str2,*m_DB);
         assertNumerateString (2);
+
         //проверяем что добавилось
-        sql_stream check("SELECT * from test_table",*m_DB);
+        sql_stream check("SELECT test_id, test_text, test_char200, TO_CHAR(test_date,'YYYYMMDDHH24MISS'), test_int_not_null FROM test_table",*m_DB);
         int id, int_not_null;
-        QString text, char200, date;
-        check >> id >> text >> char200 >> date >> int_not_null;
+        QString text, char200, dateStr;
+        check >> id >> text >> char200 >> dateStr >> int_not_null;
+        date dateDate (dateStr);
         CPPUNIT_ASSERT(id == 1);
         CPPUNIT_ASSERT(text == "Insert Text 1");
         CPPUNIT_ASSERT(char200 == "Insert Te");
-        CPPUNIT_ASSERT(date != "");
+        CPPUNIT_ASSERT(!dateDate.is_null());
         CPPUNIT_ASSERT(int_not_null == 1);
-        QString date2;
-        check >> id >> text >> char200 >> date2 >> int_not_null;
+        QString dateStr2;
+        check >> id >> text >> char200 >> dateStr2 >> int_not_null;
+        date dateDate2 (dateStr2);
         CPPUNIT_ASSERT(id == 2);
         CPPUNIT_ASSERT(text == "Insert Te");
         CPPUNIT_ASSERT(char200 == "Insert Text 2");
-        CPPUNIT_ASSERT(date != date2);
+        CPPUNIT_ASSERT(dateDate.FullOutput() == dateDate2.FullOutput());
         CPPUNIT_ASSERT(int_not_null == 2);
 
     }
@@ -69,10 +79,21 @@ void test_sql_write_fields::insert()
 
 }
 
+void test_sql_write_fields::insert_throw()
+{
+    sql_write_fields fiedls(*m_DB);
+    assertNumerateString (0);
+    fiedls.add("test_id", 1);
+    fiedls.add_if_not_zero("test_int_not_null", 0);
+    QString str =  QString("INSERT INTO test_table(%1) VALUES(%2)")
+            .arg(fiedls.fields())
+            .arg(fiedls.values());
+    sql_stream insert(str,*m_DB);
+}
+
 void test_sql_write_fields::tearDown()
 {
     sql_stream query("DELETE FROM test_table", *m_DB);
-    sql_stream reset_id("ALTER SEQUENCE sequence_id RESTART with 2", *m_DB);
 }
 
 void test_sql_write_fields::assertNumerateString(int num)
--- test/testSqlWriteFields.h
+++ test/testSqlWriteFields.h
@@ -5,17 +5,21 @@
 #include <tr1/memory>
 
 class db_cnx;
+class db_excpt;
 
 class test_sql_write_fields : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(test_sql_write_fields);
     CPPUNIT_TEST(insert);
+    CPPUNIT_TEST_EXCEPTION(insert_throw, db_excpt);
     CPPUNIT_TEST_SUITE_END();
 private:
     std::tr1::shared_ptr<db_cnx> m_DB;
 public:
     test_sql_write_fields();
+    void setUp();
     void insert();
+    void insert_throw();
     void tearDown();
 private:
     void assertNumerateString (int num);
